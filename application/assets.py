cb0506e5612b6e3beaa7395e1946bca0d7f3ec5d
caschern <84686135+caschern@users.noreply.github.com>
2022-01-08 15:44:29 -0500

    update

--- application/assets.py
+++ application/assets.py
@@ -13,10 +13,10 @@ def compile_static_assets(assets):
         output='dist/css/style.css',
         extra={'rel': 'stylesheet/less'}
     )
-    home_style_bundle = Bundle(
-        'home_bp/less/home.less',
+    main_style_bundle = Bundle(
+        'main_bp/less/main.less',
         filters='less,cssmin',
-        output='dist/css/home.css',
+        output='dist/css/main.css',
         extra={'rel': 'stylesheet/less'}
     )
     profile_style_bundle = Bundle(
@@ -25,19 +25,11 @@ def compile_static_assets(assets):
         output='dist/css/profile.css',
         extra={'rel': 'stylesheet/less'}
     )
-    product_style_bundle = Bundle(
-        'products_bp/less/products.less',
-        filters='less,cssmin',
-        output='dist/css/products.css',
-        extra={'rel': 'stylesheet/less'}
-    )
     assets.register('common_style_bundle', common_style_bundle)
-    assets.register('home_style_bundle', home_style_bundle)
+    assets.register('home_style_bundle', main_style_bundle)
     assets.register('profile_style_bundle', profile_style_bundle)
-    assets.register('product_style_bundle', product_style_bundle)
     if app.config['FLASK_ENV'] == 'development':
         common_style_bundle.build()
-        home_style_bundle.build()
+        main_style_bundle.build()
         profile_style_bundle.build()
-        product_style_bundle.build()
     return assets
--- application/main/main.py
+++ application/main/main.py
@@ -1,8 +1,20 @@
+import flask
 from flask import Blueprint, render_template
 from flask import current_app as app
-import flask
 import re, json
 from flask import Flask
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from wordcloud import WordCloud
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score,precision_score,recall_score,confusion_matrix,roc_curve,classification_report
+from scikitplot.metrics import plot_confusion_matrix
 
 
 # Blueprint Configuration
@@ -29,7 +41,9 @@ def main():
         list1 = re.split('\s+', user_input_text)    
         print(list1)
         j=0
+        squell_response = "so you've arrived..."
         
         return flask.render_template('main.html', 
-            input_text=user_input_text
+            input_text=user_input_text,
+            squell_response = squell_response
             )
