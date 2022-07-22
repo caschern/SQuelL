bf5842e0b1aa035e1eb95c4e58dfbc261ad7d611
Cristian C <84686135+caschern@users.noreply.github.com>
2022-07-21 21:57:53 -0400

    Update main.py

--- application/main/main.py
+++ application/main/main.py
@@ -14,23 +14,6 @@ from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 vectorizer = DictVectorizer(sparse = True)
 
-def ngram(token, n):
-    output = []
-    for i in range(n-1, len(token)):
-        ngram = ' '.join(token[i-n+1:i+1])
-        output.append(ngram)
-    return output
-
-def create_feature(text, nrange=(1, 1)):
-    text_features = []
-    text = text.lower()
-    text_alphanum = re.sub('[^a-z0-9#]', ' ', text)
-    for n in range(nrange[0], nrange[1]+1):
-        text_features += ngram(text_alphanum.split(), n)
-    text_punc = re.sub('[a-z0-9]', ' ', text)
-    text_features += ngram(text_punc.split(), 1)
-    return Counter(text_features)
-
 # Blueprint Configuration
 main_bp = Blueprint(
     'main_bp', __name__,
